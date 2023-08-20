dcbdfc708aaf7d83862183ce72a4f2aea19490b2
Irina Mitavskaya <imitavska@gmail.com>
2023-08-21 02:35:31 +0300

    Создан вспомогательный класс Application.

--- test_add_member.py
+++ test_add_member.py
@@ -6,128 +6,16 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from member import Member
+from application import Application
 
 
 class AddNewMember(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("https://localhost/addressbook/")
-
-    def login(self, username="admin", password="secret"):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_add_new_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "add new").click()
-
-    def add_new_member(self, member):
-        wd = self.wd
-        self.open_add_new_page()
-        # add new member
-        # fill in basic info
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(member.first_name)
-        wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").clear()
-        wd.find_element(By.NAME, "middlename").send_keys(member.middle_name)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(member.last_name)
-        wd.find_element(By.NAME, "nickname").click()
-        wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys(member.nickname)
-        wd.find_element(By.NAME, "title").click()
-        wd.find_element(By.NAME, "title").clear()
-        wd.find_element(By.NAME, "title").send_keys(member.title)
-        wd.find_element(By.NAME, "company").click()
-        wd.find_element(By.NAME, "company").clear()
-        wd.find_element(By.NAME, "company").send_keys(member.company)
-        wd.find_element(By.NAME, "address").click()
-        wd.find_element(By.NAME, "address").clear()
-        wd.find_element(By.NAME, "address").send_keys(member.address)
-        # fill in phone info
-        wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "home").clear()
-        wd.find_element(By.NAME, "home").send_keys(member.home_number)
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(member.mobile_number)
-        wd.find_element(By.NAME, "work").click()
-        wd.find_element(By.NAME, "work").clear()
-        wd.find_element(By.NAME, "work").send_keys(member.work_number)
-        wd.find_element(By.NAME, "fax").click()
-        wd.find_element(By.NAME, "fax").clear()
-        wd.find_element(By.NAME, "fax").send_keys(member.fax_number)
-        # fill in email info
-        wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").clear()
-        wd.find_element(By.NAME, "email").send_keys(member.email)
-        wd.find_element(By.NAME, "email2").click()
-        wd.find_element(By.NAME, "email2").clear()
-        wd.find_element(By.NAME, "email2").send_keys(member.email2)
-        wd.find_element(By.NAME, "email3").click()
-        wd.find_element(By.NAME, "email3").clear()
-        wd.find_element(By.NAME, "email3").send_keys(member.email3)
-        wd.find_element(By.NAME, "homepage").click()
-        wd.find_element(By.NAME, "homepage").clear()
-        wd.find_element(By.NAME, "homepage").send_keys(member.homepage)
-        # fill in birthday info
-        wd.find_element(By.NAME, "bday").click()
-        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(member.birthday)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[@name='bday']/option[@value='"+member.birthday+"']").click()
-        wd.find_element(By.NAME, "bmonth").click()
-        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(member.birthmonth)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[@name='bmonth']/option[@value='"+member.birthmonth+"']").click()
-        wd.find_element(By.NAME, "byear").click()
-        wd.find_element(By.NAME, "byear").clear()
-        wd.find_element(By.NAME, "byear").send_keys(member.birthyear)
-        # fill in anniversary info
-        wd.find_element(By.NAME, "aday").click()
-        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(member.annyversary_day)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[@name='aday']/option[@value='"+member.annyversary_day+"']").click()
-        wd.find_element(By.NAME, "amonth").click()
-        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(member.annyversary_month)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/select[@name='amonth']/option[@value='"+member.annyversary_month+"']").click()
-        wd.find_element(By.NAME, "ayear").click()
-        wd.find_element(By.NAME, "ayear").clear()
-        wd.find_element(By.NAME, "ayear").send_keys(member.annyversary_year)
-        # fill in secondary address
-        wd.find_element(By.NAME, "address2").click()
-        wd.find_element(By.NAME, "address2").clear()
-        wd.find_element(By.NAME, "address2").send_keys(member.second_address)
-        wd.find_element(By.NAME, "phone2").click()
-        wd.find_element(By.NAME, "phone2").clear()
-        wd.find_element(By.NAME, "phone2").send_keys(member.phone2_number)
-        wd.find_element(By.NAME, "notes").click()
-        wd.find_element(By.NAME, "notes").clear()
-        wd.find_element(By.NAME, "notes").send_keys(member.notes)
-        # submit new member creation
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[@name='submit']").click()
-        self.return_to_home_page()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "home").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+        self.app = Application()
 
     def test_add_new_member(self):
-        self.login(username="admin", password="secret")
-        self.add_new_member(Member(first_name="nfirst",
+        self.app.login(username="admin", password="secret")
+        self.app.add_new_member(Member(first_name="nfirst",
                                        middle_name="nmiddle",
                                        last_name="nlast",
                                        nickname="nnick",
@@ -151,20 +39,20 @@ class AddNewMember(unittest.TestCase):
                                        second_address="second address",
                                        phone2_number="12 bld",
                                        notes="new notes"))
-        self.logout()
+        self.app.logout()
 
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
+        try: self.app.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: alert = self.wd.switch_to.alert
+        try: alert = self.app.wd.switch_to.alert
         except NoAlertPresentException as e: return False
         return True
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 
 if __name__ == "__main__":
